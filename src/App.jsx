b99a6ea1b9952b9de688fa52e0c0ddc858f863dc
rhushabh77 <rathodrhushabh@gmail.com>
2025-02-18 12:58:54 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="bg-red-600 text-white text-center text-3xl">
+        Reva Education Consultant
+      </h1>
+    </>
+  );
+}
+
+export default App;
